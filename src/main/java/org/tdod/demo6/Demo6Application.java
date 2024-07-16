e767fa46f8d9ab58a586185be93ad7a99483e3ef
Ron <rkinney@tdod.org>
2024-07-16 08:51:04 -0600

    add request params

--- src/main/java/org/tdod/demo6/Demo6Application.java
+++ src/main/java/org/tdod/demo6/Demo6Application.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tdod.demo6.service.DencoderService;
 
@@ -20,13 +21,13 @@ public class Demo6Application {
     }
 
     @GetMapping("/encode")
-    public String encode() {
-        return dencoderService.encode("test");
+    public String encode(@RequestParam(value = "url") String normalUrl) {
+        return dencoderService.encode(normalUrl);
     }
 
     @GetMapping("/decode")
-    public String decode() {
-        return dencoderService.decode("test");
+    public String decode(@RequestParam(value = "url") String shortenedUrl) {
+        return dencoderService.decode(shortenedUrl);
     }
 
 }
